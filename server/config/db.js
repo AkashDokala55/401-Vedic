8fb0c8aca758a394e93fb2188d345bf69c23ac41
AkashDokala55 <akashdokala7@gmail.com>
2025-10-19 22:58:57 +0530

    Fix MongoDB connection URI reference

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+const MONGO_URI="mongodb+srv://akashdokala7_db_user:Akash1255@cluster0.cd2sl2g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.MONGO_URI);
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error(err);
