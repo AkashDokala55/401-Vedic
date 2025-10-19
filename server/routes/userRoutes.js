bc8d56c76e83cb99c43b42e104d9ff400484f543
AkashDokala55 <akashdokala7@gmail.com>
2025-10-19 22:37:59 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/signup', userController.signup);
-router.post('/verify-otp', userController.verifyOtp);
-router.post('/resend-otp', userController.resendOtp);
+router.post('/verify-email-otp', userController.verifyEmailOtp);
+router.post('/resend-email-otp', userController.resendEmailOtp);
 router.post('/login', userController.login);
 
 module.exports = router;
